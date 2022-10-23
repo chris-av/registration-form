c90224c8efd29d1bc593b4d5ea326433f5946ab8
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-10-22 17:33:27 -0700

    update reducer

--- src/state/form.state.js
+++ src/state/form.state.js
@@ -8,8 +8,6 @@ export const reducer = (state, action) => {
       return { ...state, username: action.value, usernameLength: action.value.length };
     case 'setPassword':
       return { ...state, password: action.value, passwordLength: action.value.length };
-    case 'setShowPass':
-      return { ...state, showPass: !state.showPass };
     case 'setUsernameValid':
       return { ...state, usernameValid: action.value };
     case 'setPasswordValid':
@@ -28,7 +26,6 @@ export const initialState = {
   ready: false,
   username: '',
   password: '',
-  showPass: false,
   usernameValid: false,
   passwordValid: false,
   usernameLength: 0,
