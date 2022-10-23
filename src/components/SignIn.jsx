eadc07832134bba66311d8a18d76c5a12db9914e
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-10-22 17:34:34 -0700

    scaffold SignIn component

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const SignIn = ({ sharedStyles }) => {
+  return (
+    <Container>
+      Hello
+    </Container>
+  );
+}
+
+
+const Container = styled.div``;
+
+
+export default SignIn;
+
